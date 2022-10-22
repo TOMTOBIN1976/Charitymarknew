9d52876766fd8221183460577052017a6f88ae47
TomTobin <tomtobin0@gmail.com>
2022-10-22 23:33:23 +0100

    Add ids to model. Update charity event store to include ids.

--- app/src/main/java/org/wit/charitymark/models/CharitymarkMemStore.kt
+++ app/src/main/java/org/wit/charitymark/models/CharitymarkMemStore.kt
@@ -2,6 +2,12 @@ package org.wit.charitymark.models
 
 import timber.log.Timber.i
 
+var lastId = 0L
+
+internal fun getId(): Long {
+    return lastId++
+}
+
 class CharitymarkMemStore : CharitymarkStore {
 
     val charitymarks = ArrayList<CharitymarkModel>()
@@ -10,11 +16,22 @@ class CharitymarkMemStore : CharitymarkStore {
         return charitymarks
     }
 
-    override fun create(charitymark: CharitymarkModel) {
-        charitymarks.add(charitymark)
+    override fun create(placemark: CharitymarkModel) {
+        placemark.id = getId()
+        charitymarks.add(placemark)
+        logAll()
     }
 
-    fun logAll() {
-        charitymarks.forEach{ i("${it}") }
+    override fun update(placemark: CharitymarkModel) {
+        var foundPlacemark: CharitymarkModel? = charitymarks.find { p -> p.id == placemark.id }
+        if (foundPlacemark != null) {
+            foundPlacemark.title = placemark.title
+            foundPlacemark.description = placemark.description
+            logAll()
+        }
+    }
+
+    private fun logAll() {
+        charitymarks.forEach { i("$it") }
     }
 }
--- app/src/main/java/org/wit/charitymark/models/CharitymarkModel.kt
+++ app/src/main/java/org/wit/charitymark/models/CharitymarkModel.kt
@@ -4,5 +4,6 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 
-data class CharitymarkModel(var title: String = "",
+data class CharitymarkModel(var id: Long = 0,
+                            var title: String = "",
                             var description: String = "") : Parcelable
